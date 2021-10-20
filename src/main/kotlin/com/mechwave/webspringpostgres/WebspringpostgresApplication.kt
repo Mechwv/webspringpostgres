a214e1d53ce1faf040c0f0d5542281477cc2ff60
Mechwv <mechwv@gmail.com>
2021-10-20 10:19:39 +0300

    Initial commit

---
+++ src/main/kotlin/com/mechwave/webspringpostgres/WebspringpostgresApplication.kt
@@ -0,0 +1,11 @@
+package com.mechwave.webspringpostgres
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebspringpostgresApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebspringpostgresApplication>(*args)
+}
---
+++ src/test/kotlin/com/mechwave/webspringpostgres/WebspringpostgresApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mechwave.webspringpostgres
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebspringpostgresApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
